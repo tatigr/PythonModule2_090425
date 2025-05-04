07aadbc9c73f41032a1d1caa68a6096729c471f8
tatigr <tatg21@gmail.com>
2025-05-04 09:57:33 +0200

    Update 07_task_func.py

--- Lesson08/practice/funcs_part1/07_task_func.py
+++ Lesson08/practice/funcs_part1/07_task_func.py
@@ -1,4 +1,16 @@
 # Напишите функцию принимающую время в секундах и возвращающую строку формата: “hh:mm:ss”,
 # где hh - часы, mm- минуты, ss - секунды.
+
+def right_format(number):
+    hours = (number % (3600 * 24)) // 3600
+    minutes = (number % 3600) // 60
+    seconds = number % 60
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+
+number = int(input("Введите число в секундах: "))
+print("Введенное количество секунд в формате “hh:mm:ss”:", right_format(number))
+
+
 # Пример:
 # 29143 секунд → 08:05:43
