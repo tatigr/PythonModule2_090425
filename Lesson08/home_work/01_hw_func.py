473d4d1f67ca3666b4636f3a3ff35a2af5b1d9e1
tatigr <tatg21@gmail.com>
2025-05-04 11:43:09 +0200

    Update 01_hw_func.py
    #print(lucky_ticket(010151)) с этими данными выдает ошибку:  print(lucky_ticket(010151))
                           ^
    SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers

--- Lesson08/home_work/01_hw_func.py
+++ Lesson08/home_work/01_hw_func.py
@@ -3,11 +3,20 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    numbers_123 = ticket_number // 1000
+    sum_123 = numbers_123 // 100 + numbers_123 // 10 % 10 + numbers_123 % 10
+    numbers_456 = ticket_number % 1000
+    sum_456 = numbers_456 // 100 + numbers_456 // 10 % 10 + numbers_456 % 10
+    if sum_123 == sum_456:
+        return f"Этот билет - счастливый"
+    return f"Этот билет не является счастливым"
 
 
 # Тестируем функцию
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+#print(lucky_ticket(010151))
+print(lucky_ticket(000000))
+print(lucky_ticket(587677))
+print(lucky_ticket(541918))
