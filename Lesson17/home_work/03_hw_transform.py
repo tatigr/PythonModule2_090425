b1c1374bff35c64b3c7ff88fb6476a79861318c8
tatigr <tatg21@gmail.com>
2025-05-17 13:45:41 +0200

    Update 03_hw_transform.py

--- Lesson17/home_work/03_hw_transform.py
+++ Lesson17/home_work/03_hw_transform.py
@@ -2,10 +2,14 @@
 # Вам нужно отфильтровать студентов, чей средний балл выше 4.5,
 # и округлить средний балл оставшихся студентов до двух знаков после запятой.
 
-students = [
-    {'name': 'Alice', 'grade': 4.8},
-    {'name': 'Bob', 'grade': 3.9},
-    {'name': 'Charlie', 'grade': 4.65},
-    {'name': 'David', 'grade': 4.2},
-    {'name': 'Eve', 'grade': 4.91}
-]
+
+students = [{'name': 'Alice', 'grade': 4.8}, {'name': 'Bob', 'grade': 3.9}, {'name': 'Charlie', 'grade': 4.65},
+             {'name': 'David', 'grade': 4.2}, {'name': 'Eve', 'grade': 4.91}]
+
+names = list(filter(lambda person: person['grade'] > 4.5, students))
+names_after_round = list(map(lambda person: {'name': person['name'], 'grade': round(person['grade'], 2)}, names))
+print(names_after_round)
+
+
+# names_after_round = list(map(lambda person: {'name': person['name'], 'grade': round(person['grade'], 1)}, names))
+# print(names_after_round)
