50549df116bebed8410332b293d296036c11f08c
tatigr <tatg21@gmail.com>
2025-05-17 14:35:02 +0200

    Update 04_hw_transform.py

--- Lesson17/home_work/04_hw_transform.py
+++ Lesson17/home_work/04_hw_transform.py
@@ -2,10 +2,12 @@
 # Вам нужно извлечь все email-адреса и привести их к нижнему регистру.
 # email-адреса содержат символ '@'.
 
-log_entries = [
-    "User logged in: john.doe@example.com",
-    "Error occurred",
-    "New user registered: Jane_Smith@Example.ORG",
-    "Another log",
-    "Contact us at support@test.co.uk"
-]
+log_entries = ["User logged in: john.doe@example.com", "Error occurred", "New user registered: Jane_Smith@Example.ORG",
+    "Another log", "Contact us at support@test.co.uk"]
+
+words = [word for data in log_entries for word in data.split()]
+# print(words)
+emails = list(filter(lambda data: '@' in data, words))
+lower_emails = list(map(lambda data: data.lower(), emails))
+
+print(lower_emails)
